c86e18f325cac9388f397231182257f424339817
johnsonhung906 <khhung906@gmail.com>
2023-02-19 16:38:42 +0800

    card 80%

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Routes, Route, useParams } from 'react-router-dom';
+import JobCard from '../components/searchpage/JobCard'
+import Container from '@mui/material/Container';
+import {jobs} from '../params'
 
 const theme = createTheme();
+
 
 export default function JobPage() {
   let { jobid } = useParams()
   console.log(jobid)
+  console.log("hi")
+  console.log(jobs)
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import JobCard from '../components/searchpage/JobCard';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useLocation } from "react-router-dom";
 import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import {jobs} from '../params'
+
 
 const theme = createTheme();
 
@@ -11,10 +15,19 @@ export default function SearchPage() {
   let query_keywords = location.state.keywords;
   let query_location = location.state.location;
   console.log(query_keywords, query_location)
+  console.log(jobs)
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
-        search result page
+        {jobs.map((data, id) => (
+            <div>
+              <JobCard 
+                key={"search-job-" + id} 
+                job_data={JSON.stringify(data)}
+              />
+              <Box  key={"job-padding-" + id}  sx={{ m: 1 }} />
+            </div>
+        ))}
         <Link href="/jobs/05" variant="body2">
           {"to job page"}
         </Link>
